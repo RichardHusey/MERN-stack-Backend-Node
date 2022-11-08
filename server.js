0093b82c5e4d16b4d109e12e3a3dd6ffcf96bb1a
Richard Husey <chrisgogo68@gmail.com>
2022-11-07 21:10:07 -0500

    hello node

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
+app.get('/node', (req, res) => res.send('Hello Node'));
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log("Server is running on port 3000..."));
